17fd2930ad7c400ffb419e74fcf935b08ed69252
brennan knopf <brennanknopf@gmail.com>
2022-03-21 12:59:30 -0600

    feat:am able to create a new date

--- backend/drf_jwt_backend/Personal_Info/urls.py
+++ backend/drf_jwt_backend/Personal_Info/urls.py
@@ -3,7 +3,7 @@ from .views import user_dater,user_emergency_contact,get_all_messages
 
 
 urlpatterns = [
-    path('<int:pk>/Dater/', user_dater),
-    path('<int:pk>/emergency_contact/', user_emergency_contact),
-    path('/messages/', get_all_messages),
+    path('Dater/<int:pk>/', user_dater),
+    path('emergency_contact/<int:pk>/', user_emergency_contact),
+    path('messages/<int:pk>/', get_all_messages),
 ]
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import useAuth from "../../hooks/useAuth";
 import MapContainer from "../../components/MapContainer/MapContainer";
 import Messages from "../../components/Messages/Messages";
-// import DisplayMessage from "../../DisplayMessages/DisplayMessage";
+import DateForm from "../../components/DateForm/Dateform";
 
 
 
@@ -13,17 +13,21 @@ const HomePage = (props) => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
+  console.log(token)
 
-
-  
-
-  
+  async function createDate(newDate){
+    console.log(newDate)
+    console.log("Token just before axios call: " , token)
+    let response = await axios.post(`http://127.0.0.1:8000/api/Personal_Info/Dater/${user.user_id}/`, newDate,  { headers: {Authorization: 'Bearer ' + token}});
+    console.log(response)
+    }
   return (
     <div className="container">
        <div class="row">
         <h1>Home Page for {user.username}!</h1>
           <Messages />
-          {/* <DisplayMessage message={props.message} /> */}
+          <DateForm createDate={createDate} user={user} />
+  
             <MapContainer  latitude={props.latitude} longitude= {props.longitude} />
           </div>
     </div>
