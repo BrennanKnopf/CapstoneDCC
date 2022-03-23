d8eb24e39397bc398f7b6f408e4d97fa3b9a4696
brennan knopf <brennanknopf@gmail.com>
2022-03-23 15:29:40 -0600

    fix: made it so that you can establish an emergency contact

--- frontend/src/components/DateForm/Dateform.js
+++ frontend/src/components/DateForm/Dateform.js
@@ -1,4 +1,4 @@
-import React, {useState, useAuth} from 'react';
+import React, {useState} from 'react';
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/esm/Container';  
  
@@ -10,7 +10,8 @@ const DateForm = (props) => {
 
     const [date_info, setDateInfo] = useState('')
     const [unique_password, setUniquePassword] = useState('')
-    const [emergency_contact, setEmergencyContact] = useState('')
+    // const [emergency_contact, setEmergencyContact] = useState(props.emergency_contact)
+    // console.log(emergency_contact)
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -18,15 +19,16 @@ const DateForm = (props) => {
             user: props.user.user_id,
             date_info: date_info,
             unique_password: unique_password,
-            emergency_contact: emergency_contact,
+            // emergency_contact: emergency_contact,
             
 
 
         };
+        // props.find_user(emergency_contact)
         props.createDate(newDate);
         setDateInfo('');
         setUniquePassword('');
-        setEmergencyContact('');
+        // setEmergencyContact('');
     }
 
 
@@ -40,7 +42,7 @@ const DateForm = (props) => {
         <Form.Text>Date Info</Form.Text>
         <Form.Control input="post" type='text' name="unique_password" value={unique_password} onChange={(event) => setUniquePassword(event.target.value)} size='lg' />
         <Form.Text>Unique Password</Form.Text>
-        <Form.Control input="post" type='text' name="emergency_contact" value={emergency_contact} onChange={(event) => setEmergencyContact(event.target.value)} size='lg' />
+        <Form.Control input="post" type='text' name="emergency_contact" value={props.username} onChange={(event) => props.setUserName(event.target.value)} size='lg' />
         <Form.Text>Emergency Contact</Form.Text>
         <button type='submit' className='btn btn-primary'  >Start Date</button> 
         </Form.Group>
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,13 +5,15 @@ import useAuth from "../../hooks/useAuth";
 import MapContainer from "../../components/MapContainer/MapContainer";
 import DateForm from "../../components/DateForm/Dateform";
 import DateDisplay from "../../components/DateDisplay/DateDisplay";
-import EmergencyContactForm from "../../components/EmergencyContactForm/EmergencyContactForm";
+
 
 const HomePage = (props) => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
   const [user, token] = useAuth();
   const [date, setDate] = useState({})
+  const [username, setUserName] = useState('')
+  const [emergency_contact, setEmergencyContact] = useState('')
  // console.log("login credentials", user)
  // console.log("dateinformation", date)
 
@@ -22,25 +24,28 @@ const HomePage = (props) => {
   async function createDate(newDate){
     // console.log("newDate: ", newDate)
     let response = await axios.post(`http://127.0.0.1:8000/api/Personal_Info/Dater/${user.user_id}/`, newDate,  { headers: {Authorization: 'Bearer ' + token}});
+    await find_user()
     setDate(response.data)
+    
     props.getDate(response.data)
-    // if(response.status === 201){
-    //   await props.getDateInfo()
+   
+
     // } 
     
   }
 
-  async function createEmergencyContact(newEmergencyContact){
+  async function find_user(){
   
-    let response = await axios.post(`http://127.0.0.1:8000/api/Personal_Info/emergency_contact/${user.user_id}/`, newEmergencyContact, { headers: {Authorization: 'Bearer ' + token}});
+    let response = await axios.get(`http://127.0.0.1:8000/api/Personal_Info/find_user/${username}/`, { headers: {Authorization: 'Bearer ' + token}});
     console.log(response.data)
+    setEmergencyContact(response.data)
   }
+  
   return (
     <div className="container">
         <h1>Home Page for {user.username}!</h1>
-          <DateForm createDate={createDate} user={user} />
+          <DateForm createDate={createDate} user={user} find_user = {find_user} emergency_contact username={username} setUserName={setUserName}  />
           <DateDisplay  date={date} user={user} token={token} />
-          <EmergencyContactForm createEmergencyContact={createEmergencyContact} user={user} />
           <MapContainer  latitude={props.latitude} longitude= {props.longitude} />
     </div>
   );
